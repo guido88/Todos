8aa4a4e35e6c47517995148dd4be3c1eb7b90014
guido88 <guido211@hotmail.it>
2023-03-21 12:02:54 +0100

    Read me

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./TodoInput.module.css";
 
-const TodoInput = (props, ref) => {
+const TodoInput = (props) => {
   return (
     <input
       className={classes.input}
